7ce57651109bf6c71d20ef552dd16175cfbdd18a
emeryllium <YmeraldFyre@gmail.com>
2024-10-26 21:41:17 -0400

    added lexer scaffold

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod types;
+pub mod lexer;
 
 #[cfg(test)]
 mod tests {}
--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -1,14 +1,16 @@
-use lexer::types::Token;
+use lexer::{
+    lexer::Lexer, types::Token
+};
 
 pub enum Node<'a> {
     Tree(Tree<'a>),
     Token(Token<'a>),
 }
 
-pub struct Tree <'a>{
+pub struct Tree<'a>{
     children: Vec<Node<'a>>,
 }
 
-pub fn parse(tokens: Vec<Token>) -> Tree<'_> {
+pub fn parse(lexer: &Lexer) -> Tree {
     todo!()
 }
