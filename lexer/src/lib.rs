2d6df04397ffb4f2d7ad30e62c3f0f9854ca2ad2
Fejiberglibstein <amthein321@gmail.com>
2024-10-27 20:11:34 -0400

    Begin work on lexer

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,5 +1,25 @@
-pub mod types;
-pub mod lexer;
+use std::str::Chars;
 
-#[cfg(test)]
-mod tests {}
+use types::Token;
+
+mod types;
+mod errors;
+
+pub struct Lexer<'a> {
+    input: Chars<'a>,
+    position: usize,
+
+    current_char: char,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            input: input.chars(),
+            position: 0,
+            // Initialze as '\0', it gets set right when the lexer starts so it doesn't really
+            // matter
+            current_char: '\0',
+        }
+    }
+}
