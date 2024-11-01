41ac59d47599ce9666c4a544593d076f4e1b2c0f
LooserRIP <looserdeveloper@gmail.com>
2024-11-01 19:12:06 +0200

    patched up errors

--- codegen/src/errors.rs
+++ codegen/src/errors.rs
@@ -1,16 +1,45 @@
-use crate::types::Range;
+use std::fmt;
+
+use parser::parser::Node;
 
 #[derive(thiserror::Error, Debug, PartialEq)]
-#[error("Parser error at {}", range.start)]
-pub struct LexerError {
-    pub range: Range,
-    pub source: LexerErrorKind,
+#[error("Compiler error.")]
+pub struct CodegenError {
+    token: Option<ErrorToken>,
+    pub source: CodegenErrorKind,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ErrorToken {
+    pub token: Node,
+    pub position: usize
+}
+impl fmt::Display for ErrorToken {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}; {:?})", self.position, self.token)
+    }
+}
+
+impl CodegenError {
+    pub fn new(node: Node, position: usize, source: CodegenErrorKind) -> CodegenError {
+        Self {
+            token: Some(ErrorToken {
+                token: node,
+                position
+            }),
+            source
+        }
+    }
+    pub fn new_headless(source: CodegenErrorKind) -> CodegenError {
+        Self {
+            token: None,
+            source
+        }
+    }
 }
 
 #[derive(thiserror::Error, Debug, PartialEq)]
-pub enum LexerErrorKind {
-    #[error("invalid character sir")]
-    InvalidCharacter,
-    #[error("unterminated string")]
-    UnterminatedString,
+pub enum CodegenErrorKind {
+    #[error("Generic Error")]
+    Generic,
 }
