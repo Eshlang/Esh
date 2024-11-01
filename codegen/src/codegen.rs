55f133af53a2254fac826be666dd21d6a139c424
LooserRIP <looserdeveloper@gmail.com>
2024-11-01 19:02:53 +0200

    basic crate

---
+++ codegen/src/codegen.rs
@@ -0,0 +1,9 @@
+use parser::parser::Node;
+
+pub struct CodeGen {
+    pub token_tree: Vec<Node>
+}
+
+impl CodeGen {
+    
+}
---
+++ codegen/src/errors.rs
@@ -0,0 +1,16 @@
+use crate::types::Range;
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+#[error("Parser error at {}", range.start)]
+pub struct LexerError {
+    pub range: Range,
+    pub source: LexerErrorKind,
+}
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum LexerErrorKind {
+    #[error("invalid character sir")]
+    InvalidCharacter,
+    #[error("unterminated string")]
+    UnterminatedString,
+}
