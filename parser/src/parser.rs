77390a71593d1c767c9cab368646f7ea831ec515
LooserRIP <looserdeveloper@gmail.com>
2024-11-18 19:55:04 +0200

    Revert "changed access implementation in parser"
    This reverts commit 232202bf933d1442953f9e98ff3aae0d2586aa8a.

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -575,14 +575,19 @@ impl<'a> Parser<'a> {
     /// Returns the current access chain
     pub(crate) fn access(&mut self) -> Result<Node, ParserError> {
         let mut expr = self.ident()?;
-        if !self.is_at_end() && self.curr().token_type == TokenType::Dot {
-            self.advance();
-            expr = Node::Access(
-                Rc::new(expr),
-                Rc::new(self.access()?),
-            );
+        while !self.is_at_end() {
+            match self.curr().token_type {
+                TokenType::Dot => {
+                    self.advance();
+                    expr = Node::Access(
+                        Rc::new(expr), 
+                        Rc::new(self.ident()?),
+                    )
+                },
+                _ => break
+            }
         }
-        Ok(expr)
+        return Ok(expr);
     }
 
     /// Returns the current tuple
